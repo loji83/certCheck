9872c60533daf4e9bbc06d4d40e4257601a642ae
Kang <Kang@ksmartech-ui-imac.local>
2015-10-13 09:52:16 +0900

    Initial Commit

---
+++ ViewDer/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ViewDer
+//
+//  Created by Kang on 2015. 10. 13..
+//  Copyright © 2015년 Kang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
